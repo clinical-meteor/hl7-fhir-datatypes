f230d1613806db3e35b442ce4640af053ba20b68
Abigail Watson <awatson1978@gmail.com>
2016-03-15 22:15:31 -0700

    questionnaire-builder

--- package.js
+++ package.js
@@ -1,41 +1,47 @@
 Package.describe({
   name: 'clinical:hl7-resource-datatypes',
-  version: '0.1.0',
+  version: '0.2.0',
   summary: 'HL7 FHIR DataTypes - Building blocks for FHIR Resources.',
   git: 'https://github.com/clinical-meteor/hl7-resource-datatypes',
   documentation: 'README.md'
 });
 
-Package.onUse(function(api) {
+Package.onUse(function (api) {
   api.versionsFrom('1.1.0.3');
 
   api.use('aldeed:simple-schema@1.3.3');
 
-  api.addFiles('lib/Code.js');
-  api.addFiles('lib/Period.js');
   api.addFiles('lib/Address.js');
+  api.addFiles('lib/Annotation.js');
+  api.addFiles('lib/Attachment.js');
+  api.addFiles('lib/Code.js');
+  api.addFiles('lib/Coding.js');
+  api.addFiles('lib/CodableConcept.js');
+  api.addFiles('lib/Period.js');
   api.addFiles('lib/HumanName.js');
   api.addFiles('lib/ContactPoint.js');
   api.addFiles('lib/Reference.js');
-  api.addFiles('lib/Coding.js');
-  api.addFiles('lib/CodableConcept.js');
   api.addFiles('lib/Identifier.js');
   api.addFiles('lib/Group.js');
+  api.addFiles('lib/Quantity.js');
 
 
+  api.export('AddressSchema');
+  api.export('AnnotationSchema');
+  api.export('AttachmentSchema');
   api.export('Code');
+  api.export('QuantitySchema');
   api.export('HumanNameSchema');
   api.export('ReferenceSchema');
   api.export('PeriodSchema');
   api.export('CodingSchema');
   api.export('CodableConceptSchema');
-  api.export('Identifier');
+  api.export('IdentifierSchema');
   api.export('ContactPointSchema');
-  api.export('AddressSchema');
   api.export('GroupSchema');
 });
 
-Package.onTest(function(api) {
+Package.onTest(function (api) {
   api.use('tinytest');
-  api.use('clinical:hl7-resource-patient');
+  //api.use('clinical:hl7-resource-patient');
 });
