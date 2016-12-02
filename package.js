95b8c674e7d1c7c42f113eaed6abc655a2d69965
Abigail Watson <awatson1978@gmail.com>
2016-12-01 23:12:00 -0600

    bump to 0.6.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-datatypes',
-  version: '0.5.6',
+  version: '0.6.0',
   summary: 'HL7 FHIR DataTypes - Building blocks for FHIR Resources.',
   git: 'https://github.com/clinical-meteor/hl7-resource-datatypes',
   documentation: 'README.md'
@@ -68,6 +68,26 @@ Package.onUse(function (api) {
   api.export('SampledDataSchema');
   api.export('SignatureSchema');
   api.export('TimingSchema');
+
+  api.export('Address');
+  api.export('Annotation');
+  api.export('Attachment');
+  api.export('Code');
+  api.export('Quantity');
+  api.export('HumanName');
+  api.export('Reference');
+  api.export('Period');
+  api.export('Coding');
+  api.export('CodeableConcept');
+  api.export('Identifier');
+  api.export('ContactPoint');
+  api.export('Group');
+  api.export('Conformance');
+  api.export('Range');
+  api.export('Ratio');
+  api.export('SampledData');
+  api.export('Signature');
+  api.export('Timing');
 
 });
 
