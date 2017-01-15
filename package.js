a360088aaba72fc2b957632cf770fa8332418a34
Abigail Watson <awatson1978@gmail.com>
2017-01-15 16:03:48 -0600

    optional CodingSchema fields

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-datatypes',
-  version: '0.6.8',
+  version: '0.6.9',
   summary: 'HL7 FHIR DataTypes - Building blocks for FHIR Resources.',
   git: 'https://github.com/clinical-meteor/hl7-resource-datatypes',
   documentation: 'README.md'
