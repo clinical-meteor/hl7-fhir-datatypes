bb4d8d5a7ecbce2c9003430003e117580fdd41f8
Abigail Watson <awatson@mitre.org>
2023-03-14 14:15:59 -0500

    HumanName.suffix

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:hl7-resource-datatypes',
-  version: '4.0.7',
+  version: '4.0.8',
   summary: 'HL7 FHIR DataTypes - Building blocks for FHIR Resources.',
   git: 'https://github.com/clinical-meteor/hl7-resource-datatypes',
   documentation: 'README.md'
